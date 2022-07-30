d1a23689bcdf8f5dc41421937289c8018a545d4f
Michael E Adebisi <adebisienads@gmail.com>
2022-07-30 20:36:02 +0100

    ...

--- graphql/apollo.ts
+++ graphql/apollo.ts
@@ -2,8 +2,8 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
   uri:
-    process.env.NEXT_PUBLIC_SERVER_URL == 'production'
-      ? process.env.SERVER_URL
+    process.env.NODE_ENV == 'production'
+      ? process.env.NEXT_PUBLIC_SERVER_URL
       : 'http://localhost:5000/graphql',
   cache: new InMemoryCache(),
 });
